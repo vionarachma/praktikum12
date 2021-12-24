b7672d96d03435fa7a8877e2aabab0443dbf1524
vionarachma <92927492+vionarachma@users.noreply.github.com>
2021-12-24 17:25:19 +0700

    Create classNilai.py

---
+++ classNilai.py
@@ -0,0 +1,31 @@
+def hapus(self):
+        lulus
+
+    def ubah(self):
+        lulus
+
+
+# KELAS INSTANCE
+mhs1 = Nilai("12345", "Alfin Cou", "Teknik Informatika")
+Cetak("| --Tampilkan Data-- |")
+mhs1. tampilkan()
+
+# UBAH NILAI ATRIBUT
+mhs1. set_Tugas(100)
+mhs1. set_uts(80)
+mhs1. set_uas(80)
+cetak("\n| --Cetak Data Nilai-- |")
+mhs1. set_uts(50)
+mhs1. set_uas(90)
+
+cetak("\n| --Cetak Data Nilai yang sudah di ubah-- |")
+mhs1. tampilkan()
+print(("Nilai tugas :"), mhs1. get_Tugas())
+print(("Nilai uts :"), mhs1. get_uts())
+print(("Nilai uas :"), mhs1. get_uas())
+
+# TAMPILKAN NILAI AKHIR
+cetak("\n| --Tampilkan Perhitungan Nilai Akhir-- |")
+print(("Nilai Akhir :"), mhs1. cetak())
+
+mhs1. nama
